825d210334c771c55eee936133b338a03920c52a
Abhijith S <abhijithspkm@gmail.com>
2025-05-11 17:45:37 +0530

    route updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
         <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Homepage/>}></Route>
+          <Route path="/" element={<Homepage/>}></Route>
           <Route path='/challengemanagement' element={<AdminChallenges/>}/>
         </Routes>
         </BrowserRouter>
