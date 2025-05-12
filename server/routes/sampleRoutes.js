38bab04598a3da3dd340a0b98e49b8dbe4e2561f
Abhijith S <abhijithspkm@gmail.com>
2025-05-12 23:59:11 +0530

    some changes to fix the bug

--- server/routes/sampleRoutes.js
+++ server/routes/sampleRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { challengesData, InsertChallenges, UpdateData } from "../controllers/samplecontroller.js";
-
+import { challengesData, InsertChallenges, UpdateData } from "../controllers/sampleController.js";
+// import { challengesData } from "../controllers/samplecontroller";
 
 const AuthRoutes = Router();
 
